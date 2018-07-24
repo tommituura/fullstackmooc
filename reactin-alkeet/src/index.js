cc21fd52d8fb37d27317eb696c22131678db14ce
Tommi Tuura <tommi.tuura@gmail.com>
2018-07-24 15:31:52 +0300

    1.2

--- reactin-alkeet/src/index.js
+++ reactin-alkeet/src/index.js
@@ -10,11 +10,15 @@ const Otsikko = (props) => {
 const Sisalto = (props) => {
   return (
     <div>
-    <p>{props.osa1} {props.tehtavia1}</p>
-    <p>{props.osa2} {props.tehtavia2}</p>
-    <p>{props.osa3} {props.tehtavia3}</p>
+      <Osa osa={props.osa1} tehtavia={props.tehtavia1} />
+      <Osa osa={props.osa2} tehtavia={props.tehtavia2} />
+      <Osa osa={props.osa3} tehtavia={props.tehtavia3} />
     </div>
   )
+}
+
+const Osa = (props) => {
+  return <p>{props.osa} {props.tehtavia}</p>
 }
 
 const Yhteensa = (props) => {
